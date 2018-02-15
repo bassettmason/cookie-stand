7cac1e3e929cbdee68f3a0f0954fe4b7f5e0c80c
Mason Bassett <bassettmason@gmail.com>
2018-02-14 20:24:59 -0800

    the table works ya

--- js/app.js
+++ js/app.js
@@ -4,21 +4,32 @@
 var hours = ['0600', '0700', '0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600','1700', '1800','1900'];
 var stores = [];
 var totPerDay = [];
+var cookieSales = document.getElementById('cookieSales');
 
+function makeHeader() {
+        // create tr
+    var cookieSales = document.getElementById('cookieSales')
+    var trEl = document.createElement('tr');
+    cookieSales.appendChild(trEl);
+    // create th
+    var thEl = document.createElement('th');
+    // give th content (Name for an individual cat)
+    thEl.textContent = 'Locations';
+    // append the th
+    trEl.appendChild(thEl);
+    for (var i = 0; i < hours.length; i++) {                         
+        // create th
+        thEl = document.createElement('th');
+        // give th content (Color for an individual cat)
+        thEl.textContent = hours[i];
+        // append the th
+        trEl.appendChild(thEl);
+    }
+    thEl = document.createElement('th');
+    thEl.textContent = 'Daily Totals'
+    trEl.appendChild(thEl);
+};    
 
-//calculates cookies bought per hour into var cookiePerHour the push to global array 
-// function calcCookiePerHour () {
-//     for (var i = 0; i < hours.length; i++) {
-//         var cookiePerHour = 0;
-//     }   
-    
-// };
-// //calculates total cookies sold that day global var totPerDay
-// function calcTotPerDay () {
-//     for (var i = 0; i < stores.length; i++) {
-//         totPerDay += stores[i].totalCookiesSoldPerDay;
-//     }
-// }
 //functions are above this and constructor is below this
 
 //object constructor that pushes to global stores array
@@ -35,7 +46,7 @@ function Store(location, minCust, maxCust, cookieAvg){
     this.calcCustPerHour();
     this.calcCookiePerHour();
     this.pushTotal();
- 
+    this.render();
 };
 //prototype methods below this
 Store.prototype.calcCustPerHour = function() {
@@ -49,9 +60,8 @@ Store.prototype.calcCookiePerHour = function() {        //wont work now
         var hourTot = Math.ceil(this.custPerHour[i] * this.cookieAvg);
         this.totPerHour.push(hourTot);      
         this.totalCookiesSoldPerDay += hourTot;   
-          
     }
-
+        
 };
 
 Store.prototype.pushTotal= function() {
@@ -59,9 +69,27 @@ Store.prototype.pushTotal= function() {
 };
 
 Store.prototype.render = function() {
-    
+    var trEl = document.createElement('tr');
+    cookieSales.appendChild(trEl);
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.location;
+    trEl.appendChild(tdEl);
+    for (var i = 0; i < hours.length; i++) {
+        var tdEl = document.createElement('td');
+        tdEl.textContent = this.totPerHour[i];
+        trEl.appendChild(tdEl);
+    }
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.totalCookiesSoldPerDay;
+    trEl.appendChild(tdEl);
+       
+
+
+
+  
 };
 
+makeHeader();
 //call constructor to create stores
 new Store('1st and pike', 23, 65, 6.3);
 new Store('Seatac Airport', 3, 24, 1.2);
